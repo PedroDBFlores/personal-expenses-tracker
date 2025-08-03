c8f2e81296ffedf5d998126f5f1be92b1e6f3def
Pedro Flores <pedro_flores_16@hotmail.com>
2025-08-03 10:07:44 +0100

    Added all the required endpoints to the routes

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -3,29 +3,34 @@ package routes
 import (
 	"pedro/personal-expenses-tracker/adapters/database"
 	"pedro/personal-expenses-tracker/adapters/web/handlers"
-	"pedro/personal-expenses-tracker/ports"
 	"pedro/personal-expenses-tracker/usecases"
 
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
 )
 
 func Setup(app *fiber.App) {
 	dialector := sqlite.Open("expenses.db")
 	db, _ := database.ConnectDatabase(dialector)
 
+	// Build usecases
+	getExpensesUC := usecases.NewGetExpensesUseCase(db)
+	createExpensesUC := usecases.NewCreateExpenseUseCase(db)
+	searchExpensesUC := usecases.NewSearchExpensesUseCase(db)
+	updateExpensesUC := usecases.NewUpdateExpenseUseCase(db)
+	deleteExpensesUC := usecases.NewDeleteExpenseUseCase(db)
+
+	// Build handlers
+	getExpensesHandler := handlers.NewGetExpensesHandler(getExpensesUC)
+	createExpensesHandler := handlers.NewCreateExpenseHandler(createExpensesUC)
+	searchExpensesHandler := handlers.NewSearchExpensesHandler(searchExpensesUC)
+	updateExpensesHandler := handlers.NewUpdateExpenseHandler(updateExpensesUC)
+	deleteExpensesHandler := handlers.NewDeleteExpenseHandler(deleteExpensesUC)
+
 	api := app.Group("/expenses")
-	getExpensesHandler := handlers.NewGetExpensesHandler(getExpenseGetter(db))
-	createExpensesHandler := handlers.NewCreateExpenseHandler(getExpenseCreator(db))
 	api.Get("/", getExpensesHandler.Handle)
 	api.Post("/", createExpensesHandler.Handle)
-}
-
-func getExpenseGetter(db *gorm.DB) ports.GetExpensesPort {
-	return usecases.NewGetExpensesUseCase(db)
-}
-
-func getExpenseCreator(db *gorm.DB) ports.CreateExpensePort {
-	return usecases.NewCreateExpenseUseCase(db)
+	api.Get("/search", searchExpensesHandler.Handle)
+	api.Put("/:id", updateExpensesHandler.Handle)
+	api.Delete("/:id", deleteExpensesHandler.Handle)
 }
